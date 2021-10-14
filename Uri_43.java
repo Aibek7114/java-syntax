260ae9bec86411e3ad427c76f1d5c694ba6c4bc3
Sharmont <aibek7114@gmail.com>
2021-10-14 20:23:35 +0600

    save

--- Uri_43.java
+++ Uri_43.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 
-
 public class Uri_43 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
@@ -9,11 +8,10 @@ public class Uri_43 {
 		double b = scan.nextDouble();
 		double c = scan.nextDouble();
 
-		if (a < b + c || b < a + c || c < a + b);
-		  x a+b/2*c
-			
-		
-
-
+		if ((a < b + c) && (b < (a + c)) && (c < (b + a))) {
+			System.out.printf("Perimetro = %.1f%n", (a + b + c));
+		} else {
+			System.out.printf("Area = %.1f%n", (a + b) / 2 * c);
+		}
 	}
 }
